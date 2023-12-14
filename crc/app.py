61640bf4eb163cfc76a1a820444d0c4a7ae545fc
Jesus Gastelum <jgastelum@pinnacleaerospace.com>
2023-12-14 12:00:57 -0700

    fix: fixing typo, jagp

--- crc/app.py
+++ crc/app.py
@@ -9,12 +9,14 @@ def index():
 @app.route("/hello")
 def greating():
     return "hello you!"
-
+# http://localhost:4000/sum/100/153
 @app.route("/sum/<int:a>/<int:b>")
 def sum(a: int, b: int):
     result_sum = a + b
     return str(result_sum)
-    
-@app.route("/multiply/<int:c>/<int:d>")
+
+#http://localhost:4000/multiply/20.4/15.3
+@app.route("/multiply/<float:c>/<float:d>")
 def multi(c:int, d:int):
-    return f"The result of the multiplication is:{str(float(c*d)})"
+    result_mult = c*d
+    return f"The result of the multiplication is: {str(float(result_mult))}"
